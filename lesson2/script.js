f043ca46bb211b0a0017211ea8458c338135919e
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-03-03 14:32:28 -0800

    Add event listener for form being submitted, blocking default of form submission

--- lesson2/script.js
+++ lesson2/script.js
@@ -1,3 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
   let answer = Math.floor(Math.random() * 100) + 1;
+  let input = document.querySelector('#guess');
+  let form = document.querySelector('form');
+
+  form.addEventListener('submit', e => {
+    event.preventDefault();
+    let guess = ParseInt(input.value, 10);
+    let message;
+  });
 });
