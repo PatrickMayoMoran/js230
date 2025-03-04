fad5f51b467a560d113f4466fb5ffdaa348fd2eb
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-03-03 19:26:09 -0800

    Correct errors in script for guessing game

--- lesson2/script.js
+++ lesson2/script.js
@@ -1,12 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
-  let answer = Math.floor(Math.random() * 100) + 1;
   let input = document.querySelector('#guess');
   let form = document.querySelector('form');
+  let paragraph = document.querySelector('p');
+  let link = document.querySelector('a');
+  let answer;
+  let guesses;
+
+  function newGame() {
+    answer = Math.floor(Math.random() * 100) + 1;
+    guesses = 0;
+    paragraph.textContent = 'Guess a number from 1 to 100';
+  }
 
   form.addEventListener('submit', e => {
-    event.preventDefault();
-    let guess = ParseInt(input.value, 10);
+    e.preventDefault();
+
+    let guess = parseInt(input.value, 10);
     let message;
+    guesses += 1;
+
     if (guess > answer) {
       message = "Too high!";
     } else if (guess < answer) {
@@ -14,5 +26,14 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
       message = "Correct!";
     }
+
+    paragraph.textContent = message;
   });
+
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    newGame();
+  });
+
+  newGame();
 });
