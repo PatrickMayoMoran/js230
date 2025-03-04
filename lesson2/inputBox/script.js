5e2b17a25713515fb7362fa22b92734d4fc5ad47
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-03-04 09:25:16 -0800

    Add removal of focus class on text field when document is clicked

--- lesson2/inputBox/script.js
+++ lesson2/inputBox/script.js
@@ -2,6 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
   let textField = document.querySelector(".text-field");
 
   textField.addEventListener('click', e => {
+    e.stopPropagation();
     textField.classList.add('focused');
+  });
+
+  document.addEventListener('click', e => {
+    textField.classList.remove('focused');
   });
 });
