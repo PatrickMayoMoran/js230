ea4438c36ae8c2b874819eb79547cb1f69398716
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-03-04 10:02:21 -0800

    Add clearing of text cursor when document is clicked

--- lesson2/inputBox/script.js
+++ lesson2/inputBox/script.js
@@ -1,12 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
   let textField = document.querySelector(".text-field");
+  let cursorInterval;
 
   textField.addEventListener('click', e => {
     e.stopPropagation();
     textField.classList.add('focused');
+
+    cursorInterval = setInterval(() => textField.classList.toggle('cursor'), 500);
   });
 
   document.addEventListener('click', e => {
+    clearInterval(cursorInterval);
     textField.classList.remove('focused');
+    textField.classList.remove('cursor');
   });
 });
