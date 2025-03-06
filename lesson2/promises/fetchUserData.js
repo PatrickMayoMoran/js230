35ba0464d2492672bd0f5fe28310e837c65c1303
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-03-06 14:50:52 -0800

    Add own solution to catch error for fetchUserData problem

---
+++ lesson2/promises/fetchUserData.js
@@ -0,0 +1,7 @@
+function fetchUserData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => reject({ error: "User not found" }), 500);
+  });
+}
+
+fetchUserData().catch((error) => console.log(error["error"])).finally(() => console.log("fetching complete"));
