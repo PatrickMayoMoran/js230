6c6e08743ca60413bec863417a27a157b13eedc8
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-03-08 13:19:12 -0800

    Add own solution to allSettled promise practice problem

---
+++ lesson2/promises/allSettledPromise.js
@@ -0,0 +1,16 @@
+function flakyService() {
+  return new Promise((resolve, reject) => {
+    if (Math.random() > 0.5) {
+      resolve("Operation successful");
+    } else {
+      reject("Operation failed");
+    }
+  });
+}
+
+const services = [flakyService(), flakyService(), flakyService()];
+
+Promise.allSettled(services)
+  .then((results) => {
+    results.forEach((result) => console.log(result));
+  });
